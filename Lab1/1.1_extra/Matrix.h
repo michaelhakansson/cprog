a714248364e9ff8fb61c284acff3bb1aba1fd549
michaelhakansson <michael.hakansson@gmail.com>
2014-10-01 20:14:24 +0200

    A lot of matrix stuff

--- Lab1/1.1_extra/Matrix.h
+++ Lab1/1.1_extra/Matrix.h
@@ -1,20 +1,20 @@
 #ifndef MATRIX_H
 #define MATRIX_H
-#include <vector>
+#include "Vector.h"
 #include <iostream>
 #include <stdexcept>
 
-template <class T>
-class Vector : public std::vector< T > {
-public:
-    explicit Vector <T> (std::size_t size  = 0, T data = T()) : std::vector<T>(size, data) {}
-    const T& operator[](unsigned int i) const throw(std::out_of_range) {
-        return this->at( i );
-    }
-    T& operator[](unsigned int i) throw(std::out_of_range) {
-        return this->at( i );
-    }    
-};
+// template <class T>
+// class Vector : public std::vector< T > {
+// public:
+//     explicit Vector <T> (std::size_t size  = 0, T data = T()) : std::vector<T>(size, data) {}
+//     const T& operator[](unsigned int i) const throw(std::out_of_range) {
+//         return this->at( i );
+//     }
+//     T& operator[](unsigned int i) throw(std::out_of_range) {
+//         return this->at( i );
+//     }    
+// };
 
 //using namespace std;
 
@@ -35,7 +35,7 @@ class Matrix
     Matrix( );
     Matrix( std::size_t, std::size_t );
     Matrix( const Matrix& );
-    Matrix(int size);
+    Matrix(std::size_t size);
     ~Matrix( );
     
     Matrix& operator= ( const Matrix& );
@@ -55,7 +55,7 @@ class Matrix
     
  protected:
  private:
-    Vector< matrix_row >        m_vectors;
+    Vector< matrix_row >        *m_vectors;
     std::size_t                 m_rows;
     std::size_t                 m_cols;
     
@@ -63,8 +63,109 @@ class Matrix
     friend std::istream& operator>> ( std::istream&, Matrix& );
 };
 
+
+Matrix::Matrix( ) : Matrix::Matrix(0) {
+    // Delegate
+}
+
+Matrix::Matrix(std::size_t num_rows, std::size_t num_columns) 
+    :   m_vectors(new Vector<matrix_row>(num_rows, num_columns)),
+        m_rows(num_rows),
+        m_cols(num_columns) {
+
+    for (std::size_t i = 0; i < m_rows; ++i) {
+        for (std::size_t j = 0; j < m_cols; ++j) {
+            (*m_vectors)[i][j] = 0;
+        }
+    }
+}
+
+// Copy constructor
+Matrix::Matrix(const Matrix& src)
+    :   m_vectors(new Vector<matrix_row>(src.m_rows, src.m_cols)),
+        m_rows(src.m_rows),
+        m_cols(src.m_cols) {
+
+    for (std::size_t i = 0; i < m_rows; ++i) {
+        for (std::size_t j = 0; j < m_cols; ++j) {
+            (*m_vectors)[i][j] = (*src.m_vectors)[i][j];
+        }
+    }
+}
+
+
+// Constructor for nxn id-matrix
+Matrix::Matrix(std::size_t size)
+    :   m_vectors( new Vector<matrix_row>(size, size) ),
+        m_rows(size),
+        m_cols(size) {
+
+    for (std::size_t i = 0; i < m_rows; ++i) {
+        for (std::size_t j = 0; j < m_cols; ++j) {
+            (*m_vectors)[i][j] = (j == i ? 1 : 0);
+        }
+    }
+
+}
+
+// Destructor
+Matrix::~Matrix( ) {
+    delete m_vectors;
+}
+
+// Copy assignment operator
+Matrix& Matrix::operator= ( const Matrix& src ) {
+    if (this != &src) {
+        delete m_vectors;
+        m_vectors = new Vector<matrix_row>(src.m_rows, src.m_cols);
+        m_rows = src.m_rows;
+        m_cols = src.m_cols;
+        for (std::size_t i = 0; i < m_rows; ++i) {
+            for (std::size_t j = 0; j < m_cols; ++j) {
+                (*m_vectors)[i][j] = (*src.m_vectors)[i][j];
+            }
+        }
+    }
+    return *this;
+}
+
+Matrix::matrix_row& Matrix::operator[]( index i ) {
+    if(i >= m_rows) {
+        throw std::out_of_range("Index out of bounds");
+    }
+    return (*m_vectors)[i];
+}
+
+const Matrix::matrix_row& Matrix::operator[]( index i ) const {
+    if(i >= m_rows) {
+        throw std::out_of_range("Index out of bounds");
+    }
+    return (*m_vectors)[i];
+}
+
+
 std::istream& operator>> ( std::istream&, Matrix& );
-std::ostream& operator<< ( std::ostream&, Matrix& );
+
+std::ostream& operator<< ( std::ostream& output, Matrix& matrix) {
+    output << "[ ";
+    for (std::size_t i = 0; i < matrix.rows(); ++i) {
+        output << (i == 0 ? "" : "\n; ");
+        for (std::size_t j = 0; j < matrix.cols(); ++j) {
+            output << matrix[i][j] << " ";
+        }
+    }
+    output << "]";
+    return output;
+}
+
 Matrix operator* ( int, const Matrix& );
+
+std::size_t Matrix::rows() const {
+    return m_rows;
+}
+
+std::size_t Matrix::cols() const {
+    return m_cols;
+}
 
 #endif // MATRIX_H
