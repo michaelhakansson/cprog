7ab80ed5cc813cc8af38b4c9dfa3f4e26028a4bc
adrianblp <adrianblp@gmail.com>
2014-12-04 11:58:13 +0100

    work on environment

--- Lab3/world.cpp
+++ Lab3/world.cpp
@@ -5,9 +5,9 @@ void World::init() {
 
 	// Generate map
     starting_environment_ = new Environment();
-    //environments_.push_back(starting_environment_);
+    environments_.push_back(starting_environment_);
 
 	// Place characters inside maps
     main_character_ = new Character();
-    //main_character_->enter(starting_environment_);
+    main_character_->enter(starting_environment_);
 }
