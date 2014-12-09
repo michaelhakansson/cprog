e64d69ce7872ed092a5ea4909fd3e00c7b97316b
adrianblp <adrianblp@gmail.com>
2014-12-09 15:04:43 +0100

    working on moving in the correct directions

--- Lab3/environment.cpp
+++ Lab3/environment.cpp
@@ -5,9 +5,18 @@ namespace jonsson_league {
     Environment::Environment() {
         description_ = "unknown";
         type_ = "unknown";
+		neighbours_ = new std::vector<Environment * >(4, NULL);
+		items_ = new std::vector<Item*>;
     }
 
-    Environment::Environment(std::vector<Item*> & items, std::vector<Environment> & neighbours, std::string description, std::string type){
+	Environment::Environment(std::string description, std::string type){
+		description_ = description;
+		type_ = type;
+		neighbours_ = new std::vector<Environment * >(4, NULL);
+		items_ = new std::vector<Item*>;
+	}
+
+    Environment::Environment(std::vector<Item*> * items, std::vector<Environment*> * neighbours, std::string description, std::string type){
 
         //OBS! Shared pointers?
         items_ = items;
@@ -16,17 +25,17 @@ namespace jonsson_league {
         type_ = type;
     }
 
-    std::vector<Item*> Environment::items() const {
+    std::vector<Item*> * Environment::items() const {
     	return items_;
     }
 
     std::string Environment::item_string() const {
         std::stringstream ss;
-        for (unsigned int i = 0; i < items().size(); ++i) {
+        for (unsigned int i = 0; i < items()->size(); ++i) {
             if (i > 0) {
                  ss << ", ";
             }
-            ss << (items().at(i))->name();
+            ss << (items()->at(i))->name();
         }
         return ss.str();
     }
@@ -41,8 +50,28 @@ namespace jonsson_league {
     	return description_;
     }
 
-    std::vector<Environment> Environment::neighbors() const {
+	//Returns a vector of environment pointers which are the neighbours at specified directions
+    std::vector<Environment *> * Environment::get_neighbours() const {
     	return neighbours_;
     }
 
+	Environment * Environment::get_neighbour(int index) const {
+		if(index < 0 || index >= (int) get_neighbours()->size()){
+			return NULL;
+		} else {
+			return (*neighbours_)[index];
+		} 
+	}
+
+	//Sets the neighbour for the specified direction
+	bool Environment::set_neighbour(int index, Environment * nb) {
+
+		if(index < 0 || index >= (int) get_neighbours()->size()){
+			return false;
+		}
+
+		(*neighbours_)[index] = nb;
+
+		return true;
+	}
 }
--- Lab3/world.cpp
+++ Lab3/world.cpp
@@ -4,6 +4,14 @@ namespace jonsson_league {
 
 	// Generates a collection of environments and characters etc
 	void World::init() {
+		
+		// Declare all the environments in the world
+		Environment * entrance = new Environment("A very dark room, you hear the faint sounds of nerds typing.", "Default");
+		// TODO: More environments
+
+		entrance->set_neighbour(NORTH, entrance);
+		entrance->set_neighbour(SOUTH, entrance);
+
 		// Declare all the items in the world
 		Item * sword = new Item("Svärdet Sivert", 1, 1);
 		Item * pants = new Item("Byxan Bosse", 1, 1);
@@ -13,15 +21,7 @@ namespace jonsson_league {
 		std::vector<Item*> items_room_0;
 		items_room_0.push_back(sword);
 		items_room_0.push_back(pants);
-		
-		// TODO: Declare all links between rooms
-		std::vector<Environment> neighbors_room_0;
-		// TODO: More links
 
-		// Declare all the environments in the world
-		Environment * entrance = new Environment(items_room_0, neighbors_room_0, "A very dark room, you hear the faint sounds of nerds typing.", "Default");
-		// TODO: More environments
-	
 		// TODO: Put all environments in vector
 	    environments_.push_back(starting_environment_);
 	    // TODO: Push more elements
@@ -38,7 +38,7 @@ namespace jonsson_league {
 		Environment * current_environment = main_character_->get_environment();
 	    std::cout << current_environment->description() << std::endl;
 
-	    if (current_environment->items().size()) {
+	    if (current_environment->items()->size()) {
 	    	std::cout << "In this room the following item(s) can be seen: "
 	    		  << current_environment->item_string()
 	    		  << std::endl;
@@ -53,6 +53,7 @@ namespace jonsson_league {
 		// Make input upper case to remove case sensitivity
 		transform(input_direction.begin(), input_direction.end(), input_direction.begin(), toupper);
 		
+		//If the argument is valid
 		if (input_direction == "NORTH") {
 			direction = NORTH;
 		} else if (input_direction == "WEST") {
@@ -65,9 +66,18 @@ namespace jonsson_league {
 			direction = INVALID;
 		}
 		if (direction == INVALID) {
+			std::cout << "invalid direction" << std::endl;
 			return false;
 		} else {
-			main_character_ -> go(direction);
+
+			//If it's possible to enter in that direction
+			if(main_character_->get_environment()->get_neighbour(direction)){
+				main_character_ -> go(direction);
+				return true;
+			} else {
+				std::cout << "didn't find any direction" << std::endl;
+				return false;
+			}
 		}
 
 		return true;
